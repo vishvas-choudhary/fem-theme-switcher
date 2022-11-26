1b484d25f11c40a17c6ce3fdad7288d563d982d0
Vishvas Choudhary <vishvaschoudhary7@gmail.com>
2022-11-26 18:24:11 +0530

    Setting up file structure for new project

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
+// Use dart-sass for @use
+// sass.compiler = require('dart-sass');
+
+// Sass Task
+function scssTask(){
+  return src('app/scss/style.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// JavaScript Task
+function jsTask(){
+  return src('app/js/script.js', { sourcemaps: true })
+    .pipe(babel({presets: ['@babel/preset-env']}))
+    .pipe(terser())
+    .pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// Browsersync Tasks
+function browsersyncServe(cb){
+  browsersync.init({
+    server: {
+      baseDir: '.'
+    }, 
+    notify: {
+        styles: {
+            top: 'auto',
+            bottom: '0',
+        },
+    },
+  });
+  cb();
+}
+
+function browsersyncReload(cb){
+  browsersync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask(){
+  watch('*.html', browsersyncReload);
+  watch(['app/scss/**/*.scss', 'app/js/**/*.js'], series(scssTask, jsTask, browsersyncReload));
+}
+
+// Default Gulp task
+exports.default = series(
+  scssTask,
+  jsTask,
+  browsersyncServe,
+  watchTask
+);
